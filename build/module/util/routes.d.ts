b1fc83fc4a7028280ba0da8fc8f99acf4ab21262
KiryaFT <uper99@gmail.com>
2024-01-23 12:02:26 +0300

    Update contract addresses for mainnet

--- build/module/util/routes.d.ts
+++ build/module/util/routes.d.ts
@@ -1,7 +1,6 @@
 import { Pair } from '@uniswap/v2-sdk';
 import { Pool } from '@uniswap/v3-sdk';
-import { RouteWithValidQuote } from '../routers/alpha-router';
-import { MixedRoute, V2Route, V3Route } from '../routers/router';
+import { MixedRoute, RouteWithValidQuote, V2Route, V3Route } from '../routers';
 export declare const routeToString: (route: V3Route | V2Route | MixedRoute) => string;
 export declare const routeAmountsToString: (routeAmounts: RouteWithValidQuote[]) => string;
 export declare const routeAmountToString: (routeAmount: RouteWithValidQuote) => string;
